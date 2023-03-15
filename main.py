e6f11a33ab9a7952448d62246931a767eef656ba
Yash <yashrawat2002.yr@gmail.com>
2023-03-15 13:12:28 +0530

    add fps cap to the game so that it runs at same speed in all devices

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("CONNECT4")
 BLUE=(0,0,255)
+FPS = 60
 
 def main():
+    clock=pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)                         #defining the speed of while loop so that it runs at same speed in all devices
         for event in pygame.event.get():            #checks for the events in pygame
             if event.type == pygame.QUIT:
                 run=False
