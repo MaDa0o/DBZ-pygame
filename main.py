f7f4b7351647420c1fded0d631fedd0b74e9e440
Yash <yashrawat2002.yr@gmail.com>
2023-03-15 12:52:09 +0530

    [BUILD] [main]: fill screen with blue color and learn about scope of python loops

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("CONNECT4")
+BLUE=(0,0,255)
 
 def main():
     run = True
@@ -11,8 +12,10 @@ def main():
             if event.type == pygame.QUIT:
                 run=False
 
-#while loop ends here
-                pygame.quit()
+            WIN.fill(BLUE)
+            pygame.display.update()             #we have to manually update the display when we change it
+
+    pygame.quit()
 
 # this allows the file only to run directly run not when it was imported ,basically tells this is the main file
 if __name__ == "__main__":
